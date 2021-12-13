7d06239e34cbba8b9f9d6f094731b3fec7e7c72c
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-12-13 16:49:26 -0300

    tipos de datos. String, number, booleans, tupla

---
+++ Tipado/tipado.ts
@@ -0,0 +1,28 @@
+//let --> declarar la variable
+
+//tipo string
+//let name:string = "Esteban";
+
+//name = 44 En TS se puede asignar un tipo de dato diferente al asignado
+
+//tipo boolean
+// let isTrue:boolean = true;
+// let isFalse:boolean = false;
+
+//tipo number
+//let numero:number = 10;
+
+//tipo array
+// let numeros: number[] = [1,2,3,4];
+// let nombres: string[] = ["juan", "pedro", "jose"];
+
+// let coleccionNumeros: Array<number> = [1,2,3,4];
+// let coleccionNombres: Array<string> = ["juan", "pedro", "jose"];
+
+//tipo tupla
+let tupla: [number, string, boolean];
+
+tupla = [1, "Esteban", true];
+
+
+
---
+++ holaMundo.ts
@@ -0,0 +1,3 @@
+let nombre:string = "Hola mundo" //en ts los datos son "tipados" a diferencia de en js que es debilmente tipado
+
+console.log(nombre)
